c6d5b94c031536527f97e689f2c167a86632ca83
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-06-25 18:41:15 +0200

    Maquetación principal finalizada.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {Error404Component} from './components/error404/error404.component';
+import {PostFormComponent} from './components/post-form/post-form.component';
+import {PostListComponent} from './components/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/inicio'},
+  {path: 'inicio', component: PostListComponent},
+  {path: 'categoria/:titulo', component: PostListComponent},
+  {path: 'nuevo-post', component: PostFormComponent},
+  {path: '**', component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostViewComponent } from './components/post-view/post-view.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Error404Component } from './components/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostViewComponent,
+    PostFormComponent,
+    HeaderComponent,
+    FooterComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
